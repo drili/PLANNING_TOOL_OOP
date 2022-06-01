f5c832574a2242f74eec6c3c0c536e3df1bf8fd2
tommyarildslund <tommyarildslund@gmail.com>
2022-06-01 17:49:08 +0200

    Dynamic env update.

--- assets/functions/global_functions.php
+++ assets/functions/global_functions.php
@@ -1,8 +1,13 @@
 <?php
     // *** Checking if the user is logged in
     function UserLoggedInStatusCheck() {
+        // *** Determine the current environment
+        if ($_SERVER['SERVER_NAME'] == "localhost"){
+            $root_index = "http://localhost/projects/PLANNING_TOOL_OOP/user/login_user.php";
+        } else {
+            $root_index = "https://" . $_SERVER['SERVER_NAME'] . "/user/login_user.php";
+        }
         if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-            $root_index = "http://localhost/projects/PLANNING_TOOL_OOP/PLANNING_TOOL_OOP/user/login_user.php";
             //echo "<script>console.log('UserLoggedInStatusCheck Session is NOT set;');</script>";
             header("location: ".$root_index."");
             exit;
